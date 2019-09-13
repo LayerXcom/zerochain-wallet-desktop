b848287942ca83cdb3998390a0507eaf132702dd
osuketh <dish230@gmail.com>
2019-09-13 16:01:17 +0900

    Add gen_tx func

--- zerochain/native/src/helper.rs
+++ zerochain/native/src/helper.rs
@@ -1,15 +1,20 @@
 use rand::{OsRng, Rng};
-use zface::{config, term, wallet::*, error::Result, derive::*};
+use zface::{config, term, wallet::*, error::Result, derive::*, transaction::*};
 use zface::wallet::{
     config::*,
     commands::{
         Mnemonic, MnemonicType, Language, Seed, wallet_keystore_dirs, new_indexfile
     }
 };
+use polkadot_rs::Url;
 
 const TEST_PASSWORD: &'static str = "zerochain";
 const TEST_ACCOUNTNAME: &'static str = "zerochain";
+const CONF_PK_PATH: &'static str = "./params/conf_pk.dat";
+const CONF_VK_PATH: &'static str = "./params/conf_vk.dat";
+const FEE: u32 = 1;
 
+// Initialize a new wallet. Returning a generated address encoded by SS58 format.
 pub fn new_wallet() -> Result<String> {
     let rng = &mut OsRng::new().expect("should be able to construct RNG");
     let root_dir = config::get_default_root_dir();
@@ -44,6 +49,28 @@ pub fn new_wallet() -> Result<String> {
     Ok(keyfile.ss58_address)
 }
 
+// Generate a extrinsic to send confidential transactions.
+pub fn gen_tx(recipient_addr: Vec<u8>, amount: u32) -> Result<()> {
+    let rng = &mut OsRng::new().expect("should be able to construct RNG");
+    let root_dir = config::get_default_root_dir();
+    let spending_key = spending_key_from_keystore(root_dir, TEST_PASSWORD.as_bytes())?;
+    let url = Url::Local;
+
+    inner_confidential_transfer_tx(
+        spending_key,
+        &recipient_addr[..],
+        amount,
+        url,
+        CONF_PK_PATH,
+        CONF_VK_PATH,
+        rng
+    )
+}
+
+// =================
+//  Inner functions
+// =================
+
 fn get_new_keyfile<R: Rng>(
     rng: &mut R,
     password: &[u8],
@@ -65,3 +92,41 @@ fn get_new_keyfile<R: Rng>(
 
     Ok(keyfile)
 }
+
+// fn inner_confidential_transfer_tx<R: Rng>(
+//     spending_key: SpendingKey::<Bls12>,
+//     recipient_enc_key: &[u8],
+//     amount: u32,
+//     current_balance: u32,
+//     rng: &mut R,
+// ) -> Result<()> {
+//     let remaining_balance = current_balance - amount;
+//     let recipient_account = EncryptionKey::<Bls12>::read(&mut &recipient_enc_key[..], &PARAMS)?;
+//     let multi_keys = MultiEncKeys::<Bls12, Confidential>::new(recipient_account.clone());
+
+//     KeyContext::read_from_path(CONF_PK_PATH, CONF_VK_PATH)?
+//         .gen_proof(
+//             amount,
+//             FEE,
+//             remaining_balance,
+//             0,
+//             0,
+//             &spending_key,
+//             multi_keys,
+//             &enc_balance,
+//             getter::g_epoch(&api)?,
+//             rng,
+//             &PARAMS
+//         )?
+// }
+
+// fn g_epoch(current_height: String, epoch_length: String) -> Result<edwards::Point<Bls12, PrimeOrder>> {
+//     let current_epoch = hexstr_to_u64(current_height_str) / hexstr_to_u64(epoch_length_str);
+//     let g_epoch = GEpoch::group_hash(current_epoch as u32)?; // TODO
+
+//     let point = edwards::Point::<Bls12, _>::read(&mut g_epoch.as_ref(), &PARAMS)?
+//             .as_prime_order(&PARAMS)
+//             .unwrap();
+
+//     Ok(point)
+// }
