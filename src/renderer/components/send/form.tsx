305533f34d9487e091b15b0a3f6e23f41bf72280
masaru <massaru129@gmail.com>
2019-09-24 11:52:00 +0900

    remove send amount edit bug

--- src/renderer/components/send/form.tsx
+++ src/renderer/components/send/form.tsx
@@ -3,7 +3,7 @@ import * as zfaceHelper from '../../zface_helper';
 
 interface ISendFormStates {
     address: string;
-    amount: number;
+    amount: string;
 }
 
 export default class SendForm extends React.Component<{}, ISendFormStates> {
@@ -11,7 +11,7 @@ export default class SendForm extends React.Component<{}, ISendFormStates> {
         super(props);
         this.state = {
             address: '',
-            amount: 0,
+            amount: '',
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -39,17 +39,14 @@ export default class SendForm extends React.Component<{}, ISendFormStates> {
 
     private handleChange(event: React.FormEvent<HTMLInputElement>) {
         const { name, value } = event.currentTarget;
-        if (name === 'amount') {
-            this.setState({...this.state, [name]: parseFloat(value)});
-        } else {
-            this.setState({...this.state, [name]: value});
-        }
+        this.setState({...this.state, [name]: value});
     }
 
     private async handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
+        // TODO: add amount state validator
         try {
-            zfaceHelper.submit_tx(this.state.address, this.state.amount);
+            zfaceHelper.submit_tx(this.state.address, parseFloat(this.state.amount));
         } catch (error) {
             alert(error.message);
         }
